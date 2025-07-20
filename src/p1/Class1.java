f4c6bb4b45395a99aba2892005dad536c3dc5cb2
Sucheta Ghogare <suchetaghogare2@gmail.com>
2025-07-20 09:24:22 +0530

    Project Upload

---
+++ src/p1/Class1.java
@@ -0,0 +1,9 @@
+package p1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println("p1 c1 World");
+	}
+
+}
---
+++ src/p2/Class2.java
@@ -0,0 +1,8 @@
+package p2;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		System.out.println("p2 c2 World");
+	}
+}
